d5621088f4ee42869de8b65e4db088a4365b8806
Pavel Poroskov <pavl1853@gmail.com>
2017-05-15 17:13:32 +0300

    unend: relocation

--- app/reducers/stepsWithCars.js
+++ app/reducers/stepsWithCars.js
@@ -1,177 +1,23 @@
-//import { UPDATE_ALL, UPDATE_AUTO } from '../constants/ActionTypes'
 
-// const initialState =  {
-// //    "step02": { 
-// //      "a001bc": { autoid: "a001bc", stepid: "01", timestamp: 1002 },
-// //      "d007ef": { autoid: "d007ef", stepid: "01", timestamp: 1000 },
-// //      "d007ee": { autoid: "d007ee", stepid: "02", timestamp: 1003 }
-// //            }
-//     }
-
-const initialState0 =  { 
-    "CarIdAndStepId":{}, 
-    "StepIdAndCars":{}
-  }
-
-
-function calculateS(oldCarIdAndStepId, oldStepIdAndCars,newCars) {
-
-  let stepsToChange = {};
-
-// //      let arrCarIdToRemoveGlob = [];
-   let objCarIdToChangeGlob = {};
-
-//   newCars.forEach( car => {
-    
-
-  Object.keys(newCars).forEach( carid => {
-    let car = newCars[carid];
-
-    // if (!car.stepid) {
-    //   objCarIdToChangeGlob[car.carid] = "";
-    if (!car) {
-      objCarIdToChangeGlob[carid] = "";
-    }else{
-      objCarIdToChangeGlob[carid] = car.stepid;
-    }
-
-//    if (!car.stepid) {
-    if (!car) {
-      let oldStepId = oldCarIdAndStepId[carid];
-      if (oldStepId) {
-        if (oldStepId in stepsToChange) {
-          stepsToChange.[oldStepId]["remove"].push(carid);
-        }else{
-          stepsToChange.[oldStepId] = {"add":{},"remove":[carid]};
-        }
-//            arrCarIdToRemoveGlob.push(car.carid);
-      }
-      return;
-    };
-
-
-    let oldStepId = oldCarIdAndStepId[car.carid];
-    if (oldStepId && car.stepid != oldStepId) {
-      if (oldStepId in stepsToChange) {
-        stepsToChange.[oldStepId]["remove"].push(car.carid);
-      }else{
-        stepsToChange.[oldStepId] = {"add":{},"remove":[car.carid]};
-      }
-    }
-
-    if (car.stepid in stepsToChange) {
-//          stepsToChange.[car.stepid]["add"].push(car);
-      stepsToChange.[car.stepid]["add"].[car.carid] = car;
-    }else{
-      stepsToChange.[car.stepid] = {"add": { car.carid: car}, "remove":[]};
-    }
-  } );
-
-
-  Object.keys(stepsToChange).forEach( stepidToChange => {
-
-    let arrCarIdToRemove = stepsToChange[stepidToChange]["remove"];
-    let objCarsToAdd = stepsToChange[stepidToChange]["add"];
-
-    let carsFiltered = {};
-    if (stepidToChange in oldStepIdAndCars && arrCarIdToRemove) {
-//          cars = { ...state[stepidToDo] };
-      let oldCars = oldStepIdAndCars[stepidToChange];
-
-      if (arrCarIdToRemove) {        
-        let arrCarsId = Object.keys(oldCars).filter( carid => !arrCarIdToRemove.includes(carid) );
-
-        carsFiltered = arrCarsId.reduce( (objSum,carid) => {
-                                      objSum[carid] = oldCars[carid];
-                                      return objSum
-                                    }, {} );
-      };
-    };
-
-    let newCars = {};
-    if (arrCarIdToRemove && objCarsToAdd) {        
-      newCars = {...carsFiltered, ...objCarsToAdd};
-    }else if (objCarsToAdd) {
-      let oldCars = oldStepIdAndCars[stepidToChange];
-      newCars = {...oldCars, ...objCarsToAdd};
-    }else{
-      newCars = carsFiltered;
-    }
-
-//        newStateAdd = { ...newStateAdd, stepidToDo: cars };
-    stepsToChange[stepidToChange]["newCars"] = newCars;
-  });
-
-  let StepIdAndCarsAdd = Object.keys(stepsToChange).reduce( (objSum,stepidToChange) => {
-                                objSum[stepidToChange] = stepToDo[stepidToChange]["newCars"];
-                                return objSum
-                              }, {} );
-  let StepIdAndCars = { ...oldStepIdAndCars, ...StepIdAndCarsAdd };
-
-
-  // let CarIdAndStepId = {};
-  // if (arrCarIdToRemoveGlob) {
-  //   let arrCarsId = Object.keys(oldStepIdAndCars).filter( carid => !arrCarIdToRemoveGlob.includes(carid) );
-  //   let arrCarsIdFiltered = arrCarsId.map( carid => oldStepIdAndCars[carid] );
-  //   CarIdAndStepId = { ...arrCarsIdFiltered, ...objCarIdToChangeGlob };
-  // }else{
-  //   CarIdAndStepId = { ...oldStepIdAndCars, ...objCarIdToChangeGlob };
-  // }
-  let CarIdAndStepId = { ...oldStepIdAndCars, ...objCarIdToChangeGlob };
-
-  return { 
-    CarIdAndStepId, 
-    StepIdAndCars
-    };
-
-}
-
-export default function stepsWithCars0(state = initialState0, action) {
-
-  switch (action.type) {
-
-    case 'message_fullstate':
-
-      let newCars = action.data.cars;
-
-      let oldCarIdAndStepId = {};
-      let oldStepIdAndCars = {};
-
-      return calculateS( oldCarIdAndStepId, oldStepIdAndCars, newCars);
-
-    case 'message_update':
-
-      let newCars = action.data.cars;
-
-      let oldCarIdAndStepId = state["CarIdAndStepId"] || {};
-      let oldStepIdAndCars = state["StepIdAndCars"] || {};
-
-      return calculateS( oldCarIdAndStepId, oldStepIdAndCars, newCars);
-      
-    default:
-      return state
-  }
-}
-
-
-
-
-const intersectM1 = (arrChanges, stepid, objStepCars) => {
+const intersectSets = (arrChanges, stepid, objStepCars) => {
 
   let objAdd = {};
   let objRemove = {};
-//  let haveChanges = false;
+//  let objChange = {};
+
   let haveAdd = false;
   let haveRemove = false;
 
   arrChanges.forEach( change => {
     if (change.stepid == stepid) {
-      objAdd[change.carid] = stepid;
+      objAdd[change.carid] = true;
+//      objChange[change.carid] = true;
       haveAdd = true;
     }else if (change.carid in objStepCars) {
 //      objAdd[change.carid] = "";
       objRemove[change.carid] = false;
       haveRemove = true;
+//      objChange[change.carid] = false;
     };
   });
 
@@ -186,41 +32,47 @@ const intersectM1 = (arrChanges, stepid, objStepCars) => {
 
 const calculateSumChanges = ( objStepIdWithCarIds, objChanges ) => {
 
+  let objSumStepChanges = {};
   let haveChanges = false;
 
   let arrCarChanges = Object.keys(objChanges)
     .map( carid => ({carid, stepid: objChanges[carid] ? objChanges[carid].stepid : false }) );
 
-  let objSumStepChanges = {};
+  // let newStepIds = { ...(arrCarChanges.filter( obj => obj.stepid )
+  //                         .map( obj => obj.stepid )) );
 
-  let newStepIds = { ...(arrCarChanges.filter( obj => obj.stepid )
-                          .map( obj => obj.stepid )) );
+  // let objLoop = { ...(Object.keys(objStepIdWithCarIds)), ...newStepid };
 
-  let objLoop = { ...(Object.keys(objStepIdWithCarIds)), ...newStepid };
+  // Object.keys(objLoop).forEach( stepid => {
+    // if ( !objStepIdWithCarIds[stepid]) {
+    //   objSumStepChanges[stepid] = {};
+    //   haveChanges = true;
+    // };
 
-  Object.keys(objLoop).forEach( stepid => {
-    if ( !objStepIdWithCarIds[stepid]) {
-      objSumStepChanges[stepid] = {};
-      haveChanges = true;
-    };
+//    let oldCarIds = objStepIdWithCarIds[stepid] || {};
+
+  Object.keys(objStepIdWithCarIds).forEach( stepid => {
     
-    let oldCarIds = objStepIdWithCarIds[stepid] || {};
+    let oldCarIds = objStepIdWithCarIds[stepid];
 
-    let midChangeForStep = intersectM1( arrCarChanges, stepid, oldCarIds );
+    let midChangeForStep = intersectSets( arrCarChanges, stepid, oldCarIds );
     if (midChangeForStep.haveChanges) {
       haveChanges = true;
       // objSumStepChanges[stepid] = { 
       //                           ...objStepIdWithCarIds[stepid],
-      //                           ...midChangeForStep.objAdd
+      //                           ...midChangeForStep.objChange
       //                           };
 
       let newCarIds;// = {};
       if (midChangeForStep.haveRemove) {
 
         let objInit = midChangeForStep.haveAdd ? midChangeForStep.objAdd : {};
-        newCarIds = Object.keys(oldCarIds).filter( carid => !(carid in midChangeForStep.objRemove) )
-          .reduce( carid => {
-            return oldCarIds[carid];
+
+        newCarIds = Object.keys(oldCarIds)
+          .filter( carid => !(carid in midChangeForStep.objRemove) )
+          .reduce( (objSum, carid) => {
+            objSum[carid] = true;
+            return objSum;
           }, objInit );
 
       }else { //if (midChangeForStep.haveAdd) {  
@@ -229,8 +81,8 @@ const calculateSumChanges = ( objStepIdWithCarIds, objChanges ) => {
                     ...midChangeForStep.objAdd
                     };
       };
-      objSumStepChanges[stepid] = newCarIds;
 
+      objSumStepChanges[stepid] = newCarIds;
     };
   });
 
@@ -249,39 +101,43 @@ const initialState =  {
 // //            }
   }
 
-export default function stepsWithCars(state = initialState, action) {
+export default function stepIdsWithCarIds(state = initialState, action) {
 
   switch (action.type) {
 
     case 'message_fullstate':
 
-      let newCars = action.data.cars;
+      let cars = action.data.cars;
+      let steps = action.data.steps;
 
-//      let oldState = {};
-      let oldState;
-      if (0 < Object.keys(state).length) {
-        oldState = {};
-      }else{
-        oldState = state;
+      if (!steps) {
+        return initialState
       };
 
-      let res = calculateSumChanges( oldState, newCars );
+
+//        let newState = { ...steps.map( step => ({  [step.stepid]: {}  })  )  };
+
+      let newState = steps.reduce( (objSum, step) => {
+        objSum[step.stepid] = {};
+        return objSum; 
+      }, {} )
+
+      let res = calculateSumChanges( newState, cars );
+
       if (res.haveChanges) {
-//        return { ...oldState, ...res.Changes };
-        return res.Changes;
+        return { ...newState, ...res.Changes };
+//          return res.Changes;
       }else{
-        return oldState;
+        return newState;
       };
-
-      return calculateS( oldCarIdAndStepId, oldStepIdAndCars, newCars);
 
     case 'message_update':
 
-      let newCars = action.data.cars;
+      let cars = action.data.cars;
 
-      let oldState = state || {};
+      let oldState = state || initialState;
 
-      let res = calculateSumChanges( oldState, newCars );
+      let res = calculateSumChanges( oldState, cars );
       if (res.haveChanges) {
         return { ...oldState, ...res.Changes };
       }else{
--- app/selectors/selectors.js
+++ app/selectors/selectors.js
@@ -11,123 +11,6 @@ import _map from 'lodash.map'
 import { createSelector } from 'reselect'
 
 
-// function fnSortBy( sign_name, fnMinor ) {
-
-// 	var name = sign_name;
-// 	var signSort = 1;
-// 	if (sign_name.charAt(0) == '-') {
-//     	name = sign_name.slice( 1 );
-//     	signSort = -1;
-// 	}
-
-// 	return function( o, p ) {
-//     	var a, b;
-
-
-//     	if ( o && p && typeof o === 'object' && typeof p === 'object') {
-//         	a = o[name];
-//         	b = p[name];
-//         	if (a===b) {
-//             	return typeof fnMinor === 'function' ? fnMinor( o, p ) : 0;
-//         	}
-//         	if (typeof a === typeof b) {
-// //                	return a < b ? -1 : 1;
-//             	var res = a < b ? -1 : 1;
-// //                	if (name == 'edition_Red') {
-// //                    	res = -res;
-// //                	}
-//             	res = res*signSort;
-//             	return res;
-//         	}
-//         	return typeof a < typeof b ? -1 : 1;
-//     	} else {
-//         	throw {
-//             	name: 'Error',
-//             	message: 'Expected an object when sorting by ' + name
-//         	};
-//     	}
-// 	};
-// }
-
-
-// const getStepsWithAutos0 = function( stepsById, autosById ) {
-// 	let steps_ArrSorted = sortBy( stepsById, ['id'] );
-// 	let autosById0 = pickBy( autosById, (auto) => {return !!auto} );
-// 	let stepsWithAutos = steps_ArrSorted.map( step => ({
-// 		...step,
-// 		autos: sortBy( pickBy(autosById0, (auto) => { (auto.stepid==step.id) }) , ['timestamp'] )
-// 	}));
-
-// 	return stepsWithAutos;
-// }
-
-
-// const ObjToArr = function(_obj) {
-// //	return _obj.values();
-// 	return _obj.keys().map( key => _obj[key] );
-// }
-
-// const getStepsWithAutos = function( stepsById, autosById ) {
-
-// 	let steps_ArrSorted = ObjToArr(stepsById).sort( fnSort( 'id' ) );
-
-// 	let autosById0 = ObjToArr(autosById).filter( auto => !!auto );
-
-// 	let stepsWithAutos = steps_ArrSorted.map( step => ({
-// 		...step,
-// 		autos: autosById0.filter( auto => auto.stepid==step.id ).sort( fnSort( 'timestamp' ) )
-// 	}));
-
-// 	return {		
-// 		stepsWithAutos,
-// 		maxRows: Math.max( ...(stepsWithAutos.map( step => step.autos.length )) ) 
-// 	};
-// }
-
-// const getStepsWithAutos0 = function( stepsById, autosById ) {
-
-// 	let steps_ArrSorted = sortBy( stepsById, ['id'] );
-// 	let autosById0 = pickBy( autosById, (auto) => {return !!auto} );
-
-// 	let stepsWithAutos = steps_ArrSorted.map( step => ({
-// 		...step,
-// 		autos: sortBy( pickBy(autosById0, (auto) => { (auto.stepid==step.id) }) , ['timestamp'] )
-// 	}));
-
-// 	return {		
-// 		stepsWithAutos,
-// 		maxRows: Math.max( ...(stepsWithAutos.map( step => step.autos.length )) ) 
-// 	};
-// }
-
-// const getStepsWithAutos1 = function( stepsById, autosById ) {
-
-// 	let steps_ArrSorted = sortBy( stepsById, ['id'] );
-// 	let autosById_fs = sortBy( pickBy( autosById, auto => !!auto ), ['stepid', 'timestamp'] );
-// 	let autosById_gr = groupBy( autosById_fs, auto => auto.stepid );
-// //	console.log(steps_ArrSorted );
-// //	console.log(autosById_gr );
-// //	let autosById_gr_ar = groupBy( autosById_fs, auto => auto.stepid );
-
-// 	let nRows = Math.max( ...( _map( autosById_gr, autos => autos.length ) ) );
-// //	console.log('nRows ' + nRows );
-
-// 	let rowsAr = [];
-// 	let columns = steps_ArrSorted.map( step => autosById_gr[step.id] );
-// 	for (var i=0; i < nRows; i++ ) {
-// //		rowsAr.concat( [ steps_ArrSorted.map( step => { autosById_gr[step.id][ i ] }) ] );
-// //		rowsAr.push( steps_ArrSorted.map( step => { autosById_gr[step.id][ i ] }) );
-// 		rowsAr.push( columns.map(col => col[i]) );
-// 	};
-// //	console.log(rowsAr);
-// //	console.log(rowsAr[0] );
-// //	console.log(rowsAr[1] );
-
-// 	return {		
-// 		'head': steps_ArrSorted,
-// 		'rows': rowsAr
-// 	};
-// }
 
 const inp_sel_Steps = (state) => state.stepsById;
 const inp_sel_Cars = (state) => state.autosById;
@@ -171,9 +54,9 @@ const getOneColumnsForDiv = ( autosById ) => {
 	let autosById_fs = sortBy( autosById, ['timestamp'] );
 }
 
-const getAllColumnsForDiv = ( steps_ArrSorted, stepsWithCars, emptyCell ) => {
+const getColumns = ( steps_ArrSorted, stepsWithCars, emptyCell ) => {
 
-	let columns = steps_ArrSorted.map( step => getOneColumnsForDiv(stepsWithCars[step.id]) );
+	let columns = steps_ArrSorted.map( step => stepsWithCars[step.id] );
 
 //	let maxLength = Math.max( ...( _map( autosById_gr, autos => autos.length ) ) );
 	let arLen = columns.map( col => col.length );
@@ -192,7 +75,9 @@ const getAllColumnsForDiv = ( steps_ArrSorted, stepsWithCars, emptyCell ) => {
 				return newCol;
 			}
 		})
-	}
+	};
+
+	
 
 	return columns;
 }
