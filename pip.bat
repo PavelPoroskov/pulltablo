20c3bbc198d843d1024eebd43a6bd8ca5a3a03ff
Pavel Poroskov <pavl1853@gmail.com>
2017-06-14 11:30:42 +0300

    comb out

--- pip.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-rem c:\py\_bin\Python35\python.exe c:\py\_bin\Python35\Lib\site-packages\pip\__init__.py %1 %2 %3 %4 %5 %6 %7 %8
-D:\_bin\Python35-32\python.exe -m pip %1 %2 %3 %4 %5 %6 %7 %8
--- python.bat
+++
@@ -1,1 +0,0 @@
-D:\_bin\Python35-32\python.exe %1 %2 %3 %4 %5 %6 %7
